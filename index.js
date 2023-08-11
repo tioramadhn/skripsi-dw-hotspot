5e7740fa3d096b06a3887818ae907862079b7a70
tioramadhn <tioramadhntio@aapps.ipb.ac.id>
2023-08-11 17:20:30 +0700

    fix minor

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const {
 } = require("./utils/helper");
 const DbHotspotJob = require("./services/DbHotspotJob");
 const DwHotspotJob = require("./services/DwHotspotJob");
+const { configTime } = require("./utils/config");
 const prisma = new PrismaClient();
 
 const db = new DbHotspotJob(prisma);
@@ -35,7 +36,7 @@ const dbHotspotJob = async () => {
   try {
     alertText("Memulai DB Hotspot Job");
     const date = getDate(); //Mendapatkan tanggal hari ini
-    const url = setAndGetURL(date); //Memasang atribut tanggal pada endpoint sipongi
+    const url = setAndGetURL({ date }); //Memasang atribut tanggal pada endpoint sipongi
     const hotspots = await getHotspots(url); //Mengambil data hotspot
     console.log("Total hotspot : ", hotspots?.length ?? 0);
     // // Modul Database Hotspot
@@ -61,7 +62,7 @@ const dwHotspotJob = () => {
   }
 };
 // detik(0-59) menit(0-59) jam(0-23) tanggal(1-31) bulan(1-12 or names) dow(0-7 or names)
-cron.schedule("*/1 * * * *", dbHotspotJob, config);
-cron.schedule("59 23 * * *", dwHotspotJob, config);
+// cron.schedule("*/1 * * * *", dbHotspotJob, configTime);
+cron.schedule("59 23 * * *", dwHotspotJob, configTime);
 // dwHotspotJob();
-// dbHotspotJob();
+dbHotspotJob();
